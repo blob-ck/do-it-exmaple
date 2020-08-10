c8eaf5b50d88abe3f6924981b34893f1783c7e7c
hankw <hankwan777@gmail.com>
2020-08-11 00:15:36 +0900

    Set Default Value

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ChildComponent2 from './03/ChildComponent2';
+import DefaultPropsComponent from './03/DefaultPropsComponent';
 
 class App extends React.Component {
   render() {
     return (
       <div className="body">
-        <ChildComponent2 objValue={{ age: '20살' }} />
-        {/* Failed prop type: Invalid prop `objValue.age` of type `string` supplied to `ChildComponent2`, expected `number` */}
-        {/* Failed prop type: The prop `requiredStringValue` is marked as required in `ChildComponent2`, but its value is `undefined` */}
+        <DefaultPropsComponent />
       </div>
     );
   }
