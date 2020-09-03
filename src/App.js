49329f0e6c4b67ecf00361e4e3c168a7f6eb869f
hankw <hankwan777@gmail.com>
2020-09-04 00:35:02 +0900

    CSS by CDN

--- src/App.js
+++ src/App.js
@@ -1,53 +1,13 @@
 import React from 'react';
-import Input from './03/Input';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { firstName: 'Ethan', lastName: 'Hawke', age: 10 };
-    this.handleForms = this.handleForms.bind(this);
-  }
-
-  handleForms(name, value) {
-    this.setState((prev) => ({ ...prev, [name]: value }));
-  }
-
-  showNowTarget(e) {
-    console.log(`현재 타겟 : ${e.target.name}`);
-  }
-
   render() {
     return (
       <div>
-        <Input
-          type="text"
-          name="firstName"
-          value={this.state.firstName}
-          errorMessage={this.state.firstName ? '' : '이름을 입력하세요.'}
-          label="이름"
-          onChange={this.handleForms}
-          onFocus={this.showNowTarget}
-        />
-        <br />
-        <Input
-          type="text"
-          name="lastName"
-          value={this.state.lastName}
-          errorMessage={this.state.lastName ? '' : '성을 입력하세요.'}
-          label="성"
-          onChange={this.handleForms}
-          onFocus={this.showNowTarget}
-        />
-        <br />
-        <Input
-          type="number"
-          name="age"
-          value={this.state.age}
-          errorMessage={this.state.age ? '' : '나이를 입력하세요.'}
-          label="나이"
-          onChange={this.handleForms}
-          onFocus={this.showNowTarget}
-        />
+        <nav>
+          <div className="nav-wrapper">두잇! 리액트 CSS CDN 입히기</div>
+        </nav>
+        <h1>Material CSS</h1>
       </div>
     );
   }
