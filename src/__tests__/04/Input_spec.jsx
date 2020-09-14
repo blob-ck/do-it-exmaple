56e15d614e97ce4f9f573ae183ca49da7e931183
hankw <hankwan777@gmail.com>
2020-09-14 23:54:42 +0900

    set beforeEach, afterEach of jest

--- src/__tests__/04/Input_spec.jsx
+++ src/__tests__/04/Input_spec.jsx
@@ -4,9 +4,25 @@ import { shallow } from 'enzyme';
 import Input from '../../03/Input';
 
 describe('<Input>', () => {
+  // ...(renders without crashing에 관햔 예제항목)
   it('renders without crashing', () => {
     expect(() => {
-      shallow(<Input />);
+      shallow(<Input name="test_name" />);
     }).not.toThrow();
   });
+
+  it('has one element', () => {
+    const wrapper = shallow(<Input name="test_name" />);
+    expect(wrapper.length).toEqual(1);
+    expect(wrapper).toHaveLength(1);
+  });
 });
+
+/******************************************************************************************
+ * 앞으로 컴포넌트 테스트 시 컴포넌트의 상단에 아래 예제를 추가하여 화면 출력 시 오류발생여부 검사
+ ******************************************************************************************/
+// it('renders without crashing', () => {
+//   expect(() => {
+//     shallow(<Input name="test_name" />);
+//   }).not.toThrow();
+// });
--- src/setupTests.js
+++ src/setupTests.js
@@ -8,3 +8,17 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16.3';
 
 configure({ adapter: new Adapter() });
+
+//jest 의 afterEach, beforeEach 는 테스트코드 실행 후 특정 설정을 추가하고 변경된 설정을 초기화하기 위해 실행되는 함수
+afterEach(() => {
+  //console.error()함수 객체에 spyOn()함수로 추가된 가상의 감지코드 제거
+  console.error.mockClear();
+});
+
+beforeEach(() => {
+  //감시 함수인 spyOn()을 사용하여 console 객체의 error() 함수를
+  //실제 기능대신 mockImplementation 에 정의된 함수가 실행되도록 함
+  jest.spyOn(console, 'error').mockImplementation((e) => {
+    throw new Error(e);
+  });
+});
