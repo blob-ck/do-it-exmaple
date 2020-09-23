b754a43d1a4cc550cb96796c4c0e5021531cf3bb
hankw <hankwan777@gmail.com>
2020-09-23 23:49:09 +0900

    dive better than mount

--- src/__tests__/04/Text_spec.jsx
+++ src/__tests__/04/Text_spec.jsx
@@ -3,7 +3,12 @@ import { shallow, mount } from 'enzyme';
 
 import Text from '../../04/Text';
 
+/**
+ * mount 모든 하위컴포넌트 출력
+ * dive 직계 하위컴포넌트까지 출력 -> 성능면에서 이득
+ */
 describe('<Text>', () => {
+  // √ renders without crashing (6ms)
   it('renders without crashing', () => {
     expect(() => {
       shallow(<Text>테스트</Text>);
@@ -12,6 +17,16 @@ describe('<Text>', () => {
 
   it('contains <span>', () => {
     //Aphrodite 의 StyleInjection 무시(테스트환경에 없는 함수 호출 건너뛰기 ex:document.querySelector)
+    // √ contains <span> (26ms)
     expect(mount(<Text>텍스트</Text>).find('span')).toHaveLength(1);
+  });
+
+  it('contains <span>', () => {
+    expect(
+      shallow(<Text>테스트</Text>)
+        // √ contains <span> (3ms)
+        .dive()
+        .find('span'),
+    ).toHaveLength(1);
   });
 });
