19a988b64139b5aaeeca23bd1678446037f83deb
hankw <hankwan777@gmail.com>
2020-09-23 23:32:15 +0900

    setProps, simulate

--- src/__tests__/04/Input_spec.jsx
+++ src/__tests__/04/Input_spec.jsx
@@ -3,6 +3,36 @@ import { shallow } from 'enzyme';
 
 import Input from '../../03/Input';
 
+/******************************************************************************************
+ * 앞으로 컴포넌트 테스트 시 컴포넌트의 상단에 아래 예제를 추가하여 화면 출력 시 오류발생여부 검사
+ ******************************************************************************************/
+// it('renders without crashing', () => {
+//   expect(() => {
+//     shallow(<Input name="test_name" />);
+//   }).not.toThrow();
+// });
+
+/**
+ * describe
+ * it
+ * expect
+ * shallow
+ * toEqual
+ * toHaveLength
+ * toBe
+ * find
+ * prop
+ * props
+ * setProps
+ * html
+ * toContain
+ * jest.fn
+ * simulate
+ * toHaveBeenCalled
+ * toHaveBeenCalledTimes
+ * toHaveBeenCalledWith
+ */
+
 describe('<Input>', () => {
   // ...(renders without crashing에 관햔 예제항목)
   it('renders without crashing', () => {
@@ -38,13 +68,30 @@ describe('<Input>', () => {
       expect(wrapper.find('label')).toHaveLength(1);
     });
   });
-});
 
-/******************************************************************************************
- * 앞으로 컴포넌트 테스트 시 컴포넌트의 상단에 아래 예제를 추가하여 화면 출력 시 오류발생여부 검사
- ******************************************************************************************/
-// it('renders without crashing', () => {
-//   expect(() => {
-//     shallow(<Input name="test_name" />);
-//   }).not.toThrow();
-// });
+  //목표: 생명주기 함수를 검증하거나 변경된 값에 맞춰 화면출력 state를 검증
+  // => property는 컴포넌트에서 변경할 수 없음.
+  // => 1. state를 가진 상위 컴포넌트를 생성
+  // => 2. 테스트할 컴포넌트의 프로퍼티로 변경한 state를 전달
+  // setProps() 를 사용하면 위의 과정을 거치지 않고 검증 가능
+  it('renders errorMessage', () => {
+    const wrapper = shallow(<Input name="test_name" />);
+    expect(wrapper.find('.error')).toHaveLength(0);
+    const expectedErrorMessage = '옳지 못한 값이 입력되었습니다.';
+    wrapper.setProps({ errorMessage: expectedErrorMessage });
+    expect(wrapper.find('span').prop('className')).toBe('error');
+    expect(wrapper.find('.error')).toHaveLength(1);
+    expect(wrapper.html()).toContain(expectedErrorMessage);
+  });
+
+  //콜백함수 검증
+  it('calls back onChange on input change', () => {
+    const changeStub = jest.fn();
+    const wrapper = shallow(<Input name="test_name" onChange={changeStub} />);
+    expect(changeStub).not.toHaveBeenCalled();
+    const expectedTargetValue = 'updated input';
+    wrapper.find('input').simulate('change', { target: { value: expectedTargetValue } });
+    expect(changeStub).toHaveBeenCalledTimes(1);
+    expect(changeStub).toHaveBeenCalledWith('test_name', expectedTargetValue);
+  });
+});
